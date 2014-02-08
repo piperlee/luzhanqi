9e96e12dc48f15b78f63362b2c68822666c16186
Ruixue Li <lrxpnl@gmail.com>
2014-02-08 14:42:13 -0500

    new commit

--- tests/org/luzhanqi/client/LuzhanqiLogicTest.java
+++ tests/org/luzhanqi/client/LuzhanqiLogicTest.java
@@ -1,9 +1,9 @@
 package org.luzhanqi.client;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-import java.util.List;
-import java.util.Map;
+//import static com.google.common.base.Preconditions.checkArgument;
+//
+//import java.util.List;
+//import java.util.Map;
 
 import org.luzhanqi.client.GameApi.Delete;
 import org.luzhanqi.client.GameApi.Operation;
@@ -16,14 +16,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
+//
+//import com.google.common.collect.ImmutableList;
+//import com.google.common.collect.ImmutableMap;
+//import com.google.common.collect.Iterables;
+//import com.google.common.collect.Lists;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-
+@RunWith(JUnit4.class)
 public class LuzhanqiLogicTest {
+  private void assertMoveOk(VerifyMove verifyMove) {
+    VerifyMoveDone verifyDone = new LuzhanqiLogic().verify(verifyMove);
+    assertEquals(new VerifyMoveDone(), verifyDone);
+  }
 
+  private void assertHacker(VerifyMove verifyMove) {
+    VerifyMoveDone verifyDone = new LuzhanqiLogic().verify(verifyMove);
+    assertEquals(new VerifyMoveDone(verifyMove.getLastMovePlayerId(), "Hacker found"), verifyDone);
+  }
+  
   @Test
   public void test() {
     fail("Not yet implemented");
