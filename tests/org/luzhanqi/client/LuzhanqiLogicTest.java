2519d7be4cb9bb3d29929695912fadc4d3c57b26
Ruixue Li <lrxpnl@gmail.com>
2014-02-08 13:30:30 -0500

    add tests

--- tests/org/luzhanqi/client/LuzhanqiLogicTest.java
+++ tests/org/luzhanqi/client/LuzhanqiLogicTest.java
@@ -1,5 +1,14 @@
 package org.luzhanqi.client;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class LuzhanqiLogicTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
 
 }
