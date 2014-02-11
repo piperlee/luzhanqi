c6185e885be766ea0267d7b6d1dada9ce333a0ff
Ruixue Li <lrxpnl@gmail.com>
2014-02-11 03:16:48 -0500

    slight changes

--- tests/org/luzhanqi/client/LuzhanqiLogicTest.java
+++ tests/org/luzhanqi/client/LuzhanqiLogicTest.java
@@ -554,7 +554,9 @@ public class LuzhanqiLogicTest {
               1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)),
       new Set(B, ImmutableList.of(
               25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49)),     
-      new Set(D, ImmutableList.of()));
+      new Set(D, ImmutableList.of()),
+      new SetVisibility(W,ImmutableList.of(wId)),
+      new SetVisibility(B,ImmutableList.of(bId)));
   
   private final List<Operation> illInitialMoveWithExtraMovesB = ImmutableList.<Operation>of(
       // Black set turn to S, deploy pieces
@@ -598,7 +600,8 @@ public class LuzhanqiLogicTest {
           1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)),
       new Set(B, ImmutableList.of(
           25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49)),     
-      new Set(D, ImmutableList.of()));
+      new Set(D, ImmutableList.of()),
+      new Set(B, READY));
   
   private final List<Operation> setVB = ImmutableList.<Operation>of(
       new Set(TURN, B),
